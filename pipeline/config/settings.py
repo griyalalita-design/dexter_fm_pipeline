a6828cfb88fae643f3b7e7cff791daefb608f314
griyalalita-design <griya.lalita@ninjavan.co>
2026-04-30 09:23:26 +0700

    Update settings.py

--- pipeline/config/settings.py
+++ pipeline/config/settings.py
@@ -326,16 +326,14 @@ METABASE_CONFIG = {
             "url": "https://metabase.ninjavan.co/api/card/122256/query/json",
             "report_type": "fm",
             "common_params_template": [
-                {"id": "190fb3a4-e6cb-4b4e-a78b-f4acb7cc5448", "type": "category", "value": ["month"], "target": ["variable", ["template-tag", "aggr"]]},
+                {"id": "190fb3a4-e6cb-4b4e-a78b-f4acb7cc5448", "type": "category", "value": "month", "target": ["variable", ["template-tag", "aggr"]]},
                 {"id": "26473b49-9801-4240-ade1-6c07b7851c2a", "type": "date/single", "value": "start_end", "target": ["variable", ["template-tag", "start_date"]]},
                 {"id": "8d72da7f-6a48-4384-a283-a1c81db37e2d", "type": "date/single", "value": "end_date", "target": ["variable", ["template-tag", "end_date"]]},
                 {"id": "cd9e0dbe-b027-4a2a-bb92-bf8175c59aa1", "type": "number/=", "value": [1], "target": ["variable", ["template-tag", "is_fm_hub"]]},
                 {"id": "f1fca7d5-bddb-42ce-9771-1f17b2c6a1ec", "type": "string/=", "value": "driver_list", "target": ["dimension", ["template-tag", "route_driver_type"]]},
                 {"id": "f72285e9-4c7f-4b94-9b22-9cf8c929946f", "type": "number/=", "value": [1], "target": ["variable", ["template-tag", "nv_not_liable"]]},
                 {"id": "ecfc3da2-aca6-4303-bb42-aa3f9a21810d", "type": "string/contains", "value": ["B2BR"], "target": ["dimension", ["template-tag", "pickup_tags"]], "options": {"case-sensitive": False}},
-                {"id": "6980e48f-126e-48d9-a0d3-da79bbd63751", "type": "number/=", "value": "b2b_cc", "target": ["dimension", ["template-tag", "shipper_id"]]}
-                # {"id": "52e3d080-5c98-4416-aa1f-dc590efa3d3c", "type": "string/=", "value": key_shipper_list, "target": ["dimension", ["template-tag", "sf_parent_acc_id_coalesce"]]},
-                # {"id": "e6bb5201-d1b8-46aa-9b98-e2af5fe6578d", "type": "string/=", "value": ["Restock"], "target": ["dimension", ["template-tag", "sf_nv_product_line"]]},
+                {"id": "6980e48f-126e-48d9-a0d3-da79bbd63751", "type": "string/=", "value": "b2b_cc", "target": ["dimension", ["template-tag", "shipper_id"]]}
             ]
         },
 
@@ -348,7 +346,7 @@ METABASE_CONFIG = {
                 {"id": "8d72da7f-6a48-4384-a283-a1c81db37e2d", "type": "date/single", "value": "end_end", "target": ["variable", ["template-tag", "end_date"]]},
                 {"id": "cd9e0dbe-b027-4a2a-bb92-bf8175c59aa1", "type": "number/=", "value": [1], "target": ["variable", ["template-tag", "is_fm_hub"]]},
                 {"id": "f1fca7d5-bddb-42ce-9771-1f17b2c6a1ec", "type": "string/=", "value": "driver_type", "target": ["dimension", ["template-tag", "route_driver_type"]]},
-                {"id": "6980e48f-126e-48d9-a0d3-da79bbd63751", "type": "number/=", "value": "fsbd", "target":["dimension", ["template-tag", "shipper_id"]]}
+                {"id": "6980e48f-126e-48d9-a0d3-da79bbd63751", "type": "string/=", "value": "fsbd", "target":["dimension", ["template-tag", "shipper_id"]]}
                 # {"id": "452ee39f-7548-48f9-88aa-5348c9addd86", "type": "number/=", "value": parent_id_list, "target":["dimension",["template-tag","parent_id_coalesce"]]}
             ],
         },
@@ -372,6 +370,7 @@ METABASE_CONFIG = {
                 ],
                 "fsbd": [
                     {"id": "0822034a-6d21-4110-b68d-13ed9987bc95", "type": "string/=", "value": "fsbd", "target": ["dimension", ["template-tag", "shipper_id"]]},
+                    {"id": "642cb9f7-2d24-4c9f-8023-bf35bc987991", "type": "number/=", "value": ["1"], "target": ["variable", ["template-tag", "keyshipper_cutoff_flag"]]},
                 ],
             },
         },
